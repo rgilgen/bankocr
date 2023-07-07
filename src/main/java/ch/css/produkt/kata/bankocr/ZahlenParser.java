ec64b5138b37f5e6e8043c8706ee31455057bba4
adrian <adrian.sommer@css.ch>
2023-07-07 11:24:46 +0200

    Alle Zahlen erfasst

--- src/main/java/ch/css/produkt/kata/bankocr/ZahlenParser.java
+++ src/main/java/ch/css/produkt/kata/bankocr/ZahlenParser.java
@@ -19,11 +19,47 @@ public class ZahlenParser {
             new char[]{BLANK, UNDERLINE, BLANK},
             new char[]{BLANK, UNDERLINE, PIPE},
             new char[]{BLANK, UNDERLINE, PIPE});
+    private static final NummerZeichen NUMBER_FOUR = new NummerZeichen(
+            new char[]{BLANK, BLANK, BLANK},
+            new char[]{PIPE, UNDERLINE, PIPE},
+            new char[]{BLANK, BLANK, PIPE});
+    private static final NummerZeichen NUMBER_FIFE = new NummerZeichen(
+            new char[]{BLANK, UNDERLINE, BLANK},
+            new char[]{PIPE, UNDERLINE, BLANK},
+            new char[]{BLANK, UNDERLINE, PIPE});
+    private static final NummerZeichen NUMBER_SIX = new NummerZeichen(
+            new char[]{BLANK, UNDERLINE, BLANK},
+            new char[]{PIPE, UNDERLINE, BLANK},
+            new char[]{PIPE, UNDERLINE, PIPE});
+    private static final NummerZeichen NUMBER_SEVEN = new NummerZeichen(
+            new char[]{BLANK, UNDERLINE, BLANK},
+            new char[]{BLANK, BLANK, PIPE},
+            new char[]{BLANK, BLANK, PIPE});
+    private static final NummerZeichen NUMBER_EIGHT = new NummerZeichen(
+            new char[]{BLANK, UNDERLINE, BLANK},
+            new char[]{PIPE, UNDERLINE, PIPE},
+            new char[]{PIPE, UNDERLINE, PIPE});
+    private static final NummerZeichen NUMBER_NINE = new NummerZeichen(
+            new char[]{BLANK, UNDERLINE, BLANK},
+            new char[]{PIPE, UNDERLINE, PIPE},
+            new char[]{BLANK, UNDERLINE, PIPE});
+    private static final NummerZeichen NUMBER_ZERO = new NummerZeichen(
+            new char[]{BLANK, UNDERLINE, BLANK},
+            new char[]{PIPE, BLANK, PIPE},
+            new char[]{PIPE, UNDERLINE, PIPE});
 
     Map<NummerZeichen, Integer> zeichenMap = Map.of(
             NUMBER_ONE, 1,
             NUMBER_TWO, 2,
-            NUMBER_THREE, 3);
+            NUMBER_THREE, 3,
+            NUMBER_FOUR, 4,
+            NUMBER_FIFE, 5,
+            NUMBER_SIX, 6,
+            NUMBER_SEVEN, 7,
+            NUMBER_EIGHT, 8,
+            NUMBER_NINE, 9,
+            NUMBER_ZERO, 0
+    );
 
     public int parseZeichen(NummerZeichen input) {
         return zeichenMap.get(input).intValue();
