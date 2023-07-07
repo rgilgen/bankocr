1227f2fb75c198904d54d06fcfcbd3cb364b4e3c
adrian <adrian.sommer@css.ch>
2023-07-07 10:55:12 +0200

    refactoring: using constant for characters

--- src/main/java/ch/css/produkt/kata/bankocr/ZahlenParser.java
+++ src/main/java/ch/css/produkt/kata/bankocr/ZahlenParser.java
@@ -1,6 +1,9 @@
 package ch.css.produkt.kata.bankocr;
 
 public class ZahlenParser {
+    public static final char BLANK = ' ';
+    public static final char UNDERLINE = '_';
+    public static final char PIPE = '|';
     public int parseZeichen(NummerZeichen input) {
         return 1;
     }
--- src/test/java/ch/css/produkt/kata/bankocr/ZahlenParserTest.java
+++ src/test/java/ch/css/produkt/kata/bankocr/ZahlenParserTest.java
@@ -2,6 +2,8 @@ package ch.css.produkt.kata.bankocr;
 
 import org.junit.jupiter.api.Test;
 
+import static ch.css.produkt.kata.bankocr.ZahlenParser.BLANK;
+import static ch.css.produkt.kata.bankocr.ZahlenParser.PIPE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ZahlenParserTest{
@@ -12,9 +14,9 @@ class ZahlenParserTest{
         testee = new ZahlenParser();
         int expectedOut = 1;
         NummerZeichen input = new NummerZeichen(
-                new char[]{' ',' ', ' '},
-                new char[]{' ',' ', '|'},
-                new char[]{' ',' ', '|'});
+                new char[]{BLANK,BLANK, BLANK},
+                new char[]{BLANK,BLANK, PIPE},
+                new char[]{BLANK,BLANK, PIPE});
 
         int actualOut = testee.parseZeichen(input);
 
